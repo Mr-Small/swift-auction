82a68706fda35f19d03e460c81bba12b8e7d8da5
takesuke <take.suke1129@gmail.com>
2016-05-08 21:04:58 +0900

    move Carthage

---
+++ lib/SwiftAuction/logger.swift
@@ -0,0 +1,48 @@
+#!/usr/bin/swift
+
+import Foundation
+
+class Logger{
+  class func debud(
+    message: String,
+    function: String = __FUNCTION__,
+    file: String = __FILE__,
+    line: Int = __LINE__) { Logger.write("[DECUG]",message: message, function: function, file: file, line: line )};
+  class func info(
+    message: String,
+    function: String = __FUNCTION__,
+    file: String = __FILE__,
+    line: Int = __LINE__) { Logger.write("[INFO]",message: message, function: function, file: file, line: line )};
+  class func warnig(
+    message: String,
+    function: String = __FUNCTION__,
+    file: String = __FILE__,
+    line: Int = __LINE__) { Logger.write("WARNING",message: message, function: function, file: file, line: line)};
+  class func error(
+    message: String,
+    function: String = __FUNCTION__,
+    file: String = __FILE__,
+    line: Int = __LINE__) { Logger.write("ERROR",message: message, function: function, file: file, line: line)};
+  class func write(
+    loglevel: String,
+    message: String,
+    function: String,
+    file: String,
+    line: Int) {
+
+      let now = NSDate() //現在日時の取得
+      let dataFormatter = NSDateFormatter()
+      dataFormatter.locale = NSLocale(localeIdentifier: "ja_JP")
+      dataFormatter.timeStyle = .MediumStyle
+      dataFormatter.dateStyle = .MediumStyle
+      //println(dateFormatter.stringFromDate(now))
+
+      var nowdate = dataFormatter.stringFromDate(now)
+
+      var filename = file
+      if let match = filename.rangeOfString("[^/]*$", options: .RegularExpressionSearch) {
+        filename = filename.substringWithRange(match)
+      }
+      println("\(loglevel) => \"\(message)\" \(nowdate) Line:\(line) \(function) @\(filename)")
+    }
+}
